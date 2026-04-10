1832b4ba9a31b278bb118bb75d45e37175868bbf
Artur <test@test.com>
2026-04-11 00:32:21 +1000

    feat: design and database fixed with correct key

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rmipqunbwlemtqfpsxrh.supabase.co' 
-const supabaseAnonKey = 'sb_publishable_2ZOA9H58Q-EGqkF6XRFWqA_SUm8h-R3_mIDzV8Y'
+const supabaseUrl = 'https://rmipqunbwlemtqfpsxrh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtaXBxdW5id2xlbXRxZnBzeHJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTA0MjEsImV4cCI6MjA5MTM4NjQyMX0.UR8tWGUv_JN6KN-nOa12rYuwghlatSeWCBBsA4NsHOY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
